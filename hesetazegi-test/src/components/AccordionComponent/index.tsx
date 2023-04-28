c13ac9732fee06d2ce06a27c2904ef93777c859a
MehranMoalemi <mohammadmoallemi78@gmail.com>
2023-04-29 02:14:44 +0430

    AccordionComponent styles

--- hesetazegi-test/src/components/AccordionComponent/index.tsx
+++ hesetazegi-test/src/components/AccordionComponent/index.tsx
@@ -17,7 +17,7 @@ interface AccordionProps {
 
 const AccordionComponent = (props: AccordionProps) => {
   const { summary, details, id } = props
-  
+
   const jsxSummary = parse(summary)
   const jsxdetail = parse(details)
 
@@ -32,17 +32,22 @@ const AccordionComponent = (props: AccordionProps) => {
     <>
       <Accordion
         expanded={expanded === `acc${id}`}
-        onChange={(event, isExpanded) => handleChange(isExpanded, `acc${id}`)}>
+        onChange={(event, isExpanded) => handleChange(isExpanded, `acc${id}`)}
+        sx={{ borderRadius: '8px',boxShadow: '0px 4px 16px rgba(137, 137, 137, 0.1)' }}
+      >
 
         <AccordionSummary
           aria-controls={`acc${id} content`}
           id={`acc${id} header`}
-          expandIcon={<FcExpand />}>
-          <Typography>{jsxSummary}</Typography>
+          expandIcon={<FcExpand />}
+          sx={{ display: 'flex', flexDirection: 'row', direction: 'rtl',border:'0' }}
+        >
+          <Typography sx={{ textAlign: 'right' }}>{jsxSummary}</Typography>
+
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-           {jsxdetail}
+            {jsxdetail}
           </Typography>
         </AccordionDetails>
       </Accordion>
