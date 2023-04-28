664ef977b8a3f2b1dfd89068962231fc81ddb46c
MehranMoalemi <mohammadmoallemi78@gmail.com>
2023-04-29 04:21:56 +0430

    home page styles

--- hesetazegi-test/src/pages/home/index.tsx
+++ hesetazegi-test/src/pages/home/index.tsx
@@ -4,6 +4,7 @@ import { useQuery } from 'react-query'
 import AccordionComponent from '../../components/AccordionComponent';
 import { ItemType } from '../../types/itemType';
 import './home.scss'
+import {BsShare} from 'react-icons/bs'
 
 const Home = (): JSX.Element => {
 
@@ -16,14 +17,23 @@ const Home = (): JSX.Element => {
         }
     )
 
-    
+
     return (
 
         <div className='home'>
-            {data?.data.content.items.map((item: ItemType,index:number) => (
-                <AccordionComponent key={item.id} summary={item.title} details={item.description} id={item.id} index={index} />
-            ))
-            }
+            <div className='header'>
+                <h1>سوالات متداول</h1>
+                <span>
+                    <p><a>صفحه اصلی</a>/<a>سوالات متداول</a></p>
+                    <a className='share'><BsShare/></a>
+                </span>
+            </div>
+            <div className='items-grid'>
+                {data?.data.content.items.map((item: ItemType, index: number) => (
+                    <AccordionComponent key={item.id} summary={item.title} details={item.description} id={item.id} index={index} />
+                ))
+                }
+            </div>
         </div>
     )
 }
